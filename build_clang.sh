186bef986b494396697d45b4f689132d19c50ff2
Twisted Prime <36546624+TwistedPrime@users.noreply.github.com>
2020-04-28 14:30:29 +0100

    Build scripts: Fetch kernel version from Makefile
    Signed-off-by: Twisted Prime <36546624+TwistedPrime@users.noreply.github.com>

--- build_clang.sh
+++ build_clang.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-kernelname=5.7.0-rc3+
+
+# Fetch kernel version from makefile -By TwistedPrime
+
+makefile="$(pwd)/Makefile"
+
+VERSION=$(cat $makefile | head -2 | tail -1 | cut -d '=' -f2)
+PATCHLEVEL=$(cat $makefile | head -3 | tail -1 | cut -d '=' -f2)
+SUBLEVEL=$(cat $makefile | head -4 | tail -1 | cut -d '=' -f2)
+EXTRAVERSION=$(cat $makefile | head -5 | tail -1 | cut -d '=' -f2)
+VERSION=$(echo "$VERSION" | awk -v FPAT="[0-9]+" '{print $NF}')
+PATCHLEVEL=$(echo "$PATCHLEVEL" | awk -v FPAT="[0-9]+" '{print $NF}')
+SUBLEVEL=$(echo "$SUBLEVEL" | awk -v FPAT="[0-9]+" '{print $NF}')
+EXTRAVERSION=$(echo "$EXTRAVERSION" | awk -v FPAT="[0-9]+" '{print $NF}')
+
+KERNELVERSION="${VERSION}.${PATCHLEVEL}.${SUBLEVEL}${EXTRAVERSION}"
+
+#
+
 sudo cd
 export USE_CCACHE=1
 export USE_PREBUILT_CACHE=1
@@ -21,6 +38,6 @@ sudo make $THREADS modules
 sudo make $THREADS modules_install
 sudo make $THREADS install
 cd boot
-sudo mkinitramfs -ko initrd.img-$kernelname $kernelname
+sudo mkinitramfs -ko initrd.img-$KERNELVERSION $KERNELVERSION
 sudo update-grub
 echo YOU CAN REBOOT RN...
