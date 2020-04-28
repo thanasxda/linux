700f9e3bf162733d23a185cb300d0954fa991af2
Twisted Prime <36546624+TwistedPrime@users.noreply.github.com>
2020-04-28 15:49:47 +0100

    Introduce build.sh and remove unused scripts
    Signed-off-by: Twisted Prime <36546624+TwistedPrime@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -22,22 +22,18 @@ export USE_CCACHE=1
 export USE_PREBUILT_CACHE=1
 export PREBUILT_CACHE_DIR=~/.ccache
 export CCACHE_DIR=~/.ccache
-ccache -M 30G
-export KBUILD_BUILD_USER=thanas
-export KBUILD_BUILD_HOST=MLX
 THREADS=-j$(nproc --all)
-FLAGS="AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip"
-CLANG="CC=clang HOSTCC=clang"
+config=twisted_defconfig
 #VERBOSE="V=1"
 rm -rf .config
 rm -rf .config.old
-make menuconfig
+cp $config .config
 make localmodconfig
-sudo make $THREADS $VERBOSE $FLAGS $CLANG            
+#make menuconfig
+sudo make $THREADS $VERBOSE 
 sudo make $THREADS modules
 sudo make $THREADS modules_install
 sudo make $THREADS install
-cd boot
 sudo mkinitramfs -ko initrd.img-$KERNELVERSION $KERNELVERSION
 sudo update-grub
 echo YOU CAN REBOOT RN...
