7972e4be7e829eebd04bd802b8eb0681d621df8a
thanasxda <15927885+thanasxda@users.noreply.github.com>
2020-05-01 12:12:00 +0300

    script

--- build_thanas.sh
+++ build_thanas.sh
@@ -35,14 +35,17 @@ export CCACHE_DIR=~/.ccache
 ccache -M 30G
 THREADS=-j$(nproc --all)
 CLANG="CC=clang HOSTCC=clang LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf OBJSIZE=llvm-size STRIP=llvm-strip"
+#LD="LD=ld.lld"
 #VERBOSE="V=1"
+#export LD_LIBRARY_PATH="$CLANG/../lib:$CLANG/../lib64:$LD_LIBRARY_PATH"
+#export PATH="$CLANG:$PATH"
 stableconfig=thanas_defconfig
 sudo rm -rf .config
 sudo rm -rf .config.old
 cp $stableconfig .config
 make localmodconfig
 make menuconfig
-sudo make $THREADS $VERBOSE $CLANG            
+sudo make $THREADS $VERBOSE $CLANG $LD           
 sudo make $THREADS modules
 sudo make $THREADS modules_install
 sudo make $THREADS install
