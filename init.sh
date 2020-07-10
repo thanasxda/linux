72d85e0ec86f80c6a5a8114f0748fc56e34d4fa4
thanasxda <15927885+thanasxda@users.noreply.github.com>
2020-07-10 23:15:18 +0300

    regen

--- init.sh
+++ init.sh
@@ -136,6 +136,8 @@ sysctl net.ipv4.tcp_keepalive_intvl=15
 #sysctl net.ipv4.tcp_tw_recycle=1
 #sysctl net.ipv4.tcp_tw_reuse=1
 
+sysctl fs.xfs.xfssyncd_centisecs=10000
+
 ###### CPU
 ################################
 ### governor
